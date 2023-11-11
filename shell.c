4b593aa66ca2774c7c3ca8f7a2098c0774e74aba
Ogbe-cloud <ogbeedel10000@gmail.com>
2023-11-11 12:33:05 -0800

    latest commit

--- shell.c
+++ shell.c
@@ -1,21 +1,28 @@
 #include "shell.h"
 
+/**
+ * main - Entry point
+ * 
+ * Description:
+ * i@parameter:
+ * 
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
-    {
-        printUserMessage(argv[0]);
-        return 1;
-    }
+	if (argc != 2)
+	{
+		printUserMessage(argv[0]);
+		return (1);
+	}
+	char command[120];
+	char *prompt = "($)";
 
-    char command[120];
-    char *prompt = "($)";
-
-    while (1)
-    {
-	    print(prompt);
-	    fgets(command, sizeof(command), stdin);
-	    exeCommand(command);
-    }
-        return 0;
+	while (1)
+	{
+		print(prompt);
+		fgets(command, sizeof(command), stdin);
+		exeCommand(command);
+	}
+	return (0);
 }
